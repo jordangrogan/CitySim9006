fad29dafae801b9848919284ee2be179686a38de
Jordan Grogan <jordangrogan11@gmail.com>
2018-02-16 18:17:38 -0500

    Tests for ArgsChecker

--- argschecker.rb
+++ argschecker.rb
@@ -1,6 +1,13 @@
 class ArgsChecker
-  def check_args arr
-    raise "Enter a seed and only a seed" unless ARGV.count == 1
-     # etc.
+
+  def check_args(arr)
+    raise "Enter a seed and only a seed" unless arr.size == 1
+    if arr[0].is_a?(Integer)
+      seed = arr[0]
+    else
+      seed = 0
+    end
+    seed
   end
+
 end
--- city_sim_9006.rb
+++ city_sim_9006.rb
@@ -1,10 +1,9 @@
-require_relative "city"
 require_relative "argschecker"
+require_relative "city"
 
 argschecker = ArgsChecker::new
-argschecker.check_args(ARGV)
-
-prng = Random::new(ARGV[0].to_i) # If it's a string, .to_i will default to 0
+seed = argschecker.check_args(ARGV)
+prng = Random::new(seed)
 
 city = City::new
 city.run_simulation(prng)
