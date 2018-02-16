216bb3e6dd24ddfe9e7a71929ec1e86e42f4f4a9
Jordan Grogan <jordangrogan11@gmail.com>
2018-02-16 18:50:09 -0500

    Modified tests for Location class

--- location_test.rb
+++ location_test.rb
@@ -41,7 +41,7 @@ class LocationTest < Minitest::Test
   # UNIT TESTS FOR METHOD get_next_road(prng)
   # The parameter is a pseudorandom number generator, so it doesn't make sense for their to be equivalence classes
   # The pseudorandom number generator's rand function in this unit test is stubbed
-
+  # In this test, we make sure that the "random value" (which we're stubbing & passing into get_next_road(prng)) returns the correct road that was added in the correct order.
   def test_get_next_road
     prng = Minitest::Mock.new("prng")
     road1 = Minitest::Mock.new("test_road_1")
