9f1aa72708b0ae060e6f507414109ff3cfa1883e
Jordan Grogan <jordangrogan11@gmail.com>
2018-02-16 21:47:22 -0500

    Added comments and changed to snake_case for Avenue class/tests

--- avenue_test.rb
+++ avenue_test.rb
@@ -4,13 +4,14 @@ require_relative "avenue"
 
 class AvenueTest < Minitest::Test
 
-  # UNIT TESTS FOR METHOD toLocation(fromLocation)
+  # UNIT TESTS FOR METHOD to_location(from_location)
   # Equivalence classes:
   # toLocation = location1 -> returns location2
   # toLocation = location2 -> returns location3
   # toLocation = location3 -> returns nil
 
-  # If location1 is given for input toLocation, location2 is returned
+  # If location1 is given for input to_location, location2 is returned.
+  # loc1, loc2, & loc3 are doubles for Location objects.
   def test_loc1_to_loc2
     loc1 = Minitest::Mock.new("Location 1")
     loc2 = Minitest::Mock.new("Location 2")
@@ -19,7 +20,8 @@ class AvenueTest < Minitest::Test
     assert_equal loc2, avenue.to_location(loc1)
   end
 
-  # If location2 is given for input toLocation, location3 is returned
+  # If location2 is given for input to_location, location3 is returned
+  # loc1, loc2, & loc3 are doubles for Location objects.
   def test_loc2_to_loc3
     loc1 = Minitest::Mock.new("Location 1")
     loc2 = Minitest::Mock.new("Location 2")
@@ -28,7 +30,8 @@ class AvenueTest < Minitest::Test
     assert_equal loc3, avenue.to_location(loc2)
   end
 
-  # If location3 is given for input toLocation, nil is returned
+  # If location3 is given for input to_location, nil is returned
+  # loc1, loc2, & loc3 are doubles for Location objects.
   def test_loc3_nil
     loc1 = Minitest::Mock.new("Location 1")
     loc2 = Minitest::Mock.new("Location 2")
